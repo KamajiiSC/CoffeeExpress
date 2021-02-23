d192aa6c0a1b47409d11218d2a391c4b4b51c3e7
KamajiiSC <brandonlschaen@gmail.com>
2021-02-23 16:04:24 -0500

    Created basic example event post, and basic styling with border and such

--- src/components/EventsPage/EventsPage.js
+++ src/components/EventsPage/EventsPage.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './EventsPage.css';
+import specialEvent from '../../resources/special-event.svg';
 
 const EventsPage = () => (
-  <main>
-    events
+  <main id="event">
+    <img className="event-img" src={specialEvent} alt=""/>
+
+    <section className="events">
+      <ul className='event-list'>
+        <li>
+          <div className="event-post">
+            <h3 className="event-title">Slam Poetry</h3>
+            <p className="event-desc">Slam poetry night hosted by local university poetry club!</p>
+            <p className="event-date">3/25/2021</p>
+            <a href="#" className="link">Link</a>
+          </div>
+        </li>
+      </ul>
+      
+    </section>
   </main>
 );
 
