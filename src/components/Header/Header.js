0e864dafd148d1f527ef47a7b607a82fed399c3c
KamajiiSC <brandonlschaen@gmail.com>
2021-02-22 20:28:21 -0500

    Setup Header component and imported to HomePage

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h2 className="shop-name">CoffeeExpress</h2>
+    
+    <nav>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Events</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Faces</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default Header;
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const HomePage = () => (
   <div className="home">
-    <h1 className="title">CoffeeExpress</h1>
+    <Header />
   </div>
 );
 
