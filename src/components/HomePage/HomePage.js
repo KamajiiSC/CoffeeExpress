44e466551729f377252f4c10d3f2443b088d4ccf
KamajiiSC <brandonlschaen@gmail.com>
2021-02-23 14:47:29 -0500

    Setup structure of HomePage and NavLink

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const HomePage = () => (
   <main className="home">
-    home
+    <section className="left-section">
+      <h1 className="home-title">Café & <br /> Bakery</h1>
+      <p className="home-logo">Your routine is our routine</p>
+      <NavLink to="/contact">FIND US</NavLink>
+    </section>
+    
+    <section className="right-section">
+      
+    </section>
   </main>
 );
 
