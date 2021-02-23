d7250be7a22615125ead57987b49e1c4002e9fd8
KamajiiSC <brandonlschaen@gmail.com>
2021-02-23 16:12:49 -0500

    Added few more placeholders, styled space between and added spacing inside

--- src/components/EventsPage/EventsPage.js
+++ src/components/EventsPage/EventsPage.js
@@ -16,6 +16,22 @@ const EventsPage = () => (
             <a href="#" className="link event-el">Link</a>
           </div>
         </li>
+        <li>
+          <div className="event-post">
+            <h3 className="event-title event-el" >Slam Poetry</h3>
+            <p className="event-desc event-el">Slam poetry night hosted by local university poetry club!</p>
+            <p className="event-date event-el">3/25/2021</p>
+            <a href="#" className="link event-el">Link</a>
+          </div>
+        </li>
+        <li>
+          <div className="event-post">
+            <h3 className="event-title event-el" >Slam Poetry</h3>
+            <p className="event-desc event-el">Slam poetry night hosted by local university poetry club!</p>
+            <p className="event-date event-el">3/25/2021</p>
+            <a href="#" className="link event-el">Link</a>
+          </div>
+        </li>
       </ul>
       
     </section>
