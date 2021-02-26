ea17d243087e5c8d24399609d25b4cc004520f3b
KamajiiSC <brandonlschaen@gmail.com>
2021-02-26 14:52:25 -0500

    Wrapped content and spaced it

--- src/components/FacesPage/FacesPage.js
+++ src/components/FacesPage/FacesPage.js
@@ -11,21 +11,29 @@ const FacesPage = () => (
     <section className="team">
       <div className="team-card">
         <img className="team-img" src={john} alt=""/>
-        <h3 className="team-name">John</h3>
-        <p className="team-desc">A young passionate barista who stays cool under any pressure, John is a pillar for the team.</p>
-        <p className="fav-drink">Favorite drink: Venti decaf mocha, triple shot</p>
+        <div className="team-content">
+          <h3 className="team-name">John</h3>
+          <p className="team-desc">A young passionate barista who stays cool under any pressure, John is a pillar for the team.</p>
+          <p className="fav-drink">Favorite drink: Venti decaf mocha, triple shot</p>
+        </div>
       </div>
+
       <div className="team-card">
         <img className="team-img" src={diane} alt=""/>
-        <h3 className="team-name">Diane</h3>
-        <p className="team-desc">Out-going and organized, Diane stays 3 steps ahead of any order.</p>
-        <p className="fav-drink">Favorite drink: Grande cafe espresso frappucino, with whip</p>
+        <div className="team-content">
+          <h3 className="team-name">Diane</h3>
+          <p className="team-desc">Out-going and organized, Diane stays 3 steps ahead of any order.</p>
+          <p className="fav-drink">Favorite drink: Grande cafe espresso frappucino, with whip</p>
+        </div>
       </div>
+
       <div className="team-card">
         <img className="team-img" src={brett} alt=""/>
-        <h3 className="team-name">Brett</h3>
-        <p className="team-desc">A warm smile and an impressive beard to match, Brett makes sure anybody who walks in those doors finds what they're looking for.</p>
-        <p className="fav-drink">Favorite drink: Tall decaf black coffee</p>
+        <div className="team-content">
+          <h3 className="team-name">Brett</h3>
+          <p className="team-desc">A warm smile and an impressive beard to match, Brett makes sure anybody who walks in those doors finds what they're looking for.</p>
+          <p className="fav-drink">Favorite drink: Tall decaf black coffee</p>
+        </div>
       </div>
     </section>
   </main>
