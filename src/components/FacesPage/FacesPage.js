596cc724440fbbe3fef061f00cb6f0fe260784a2
KamajiiSC <brandonlschaen@gmail.com>
2021-02-26 14:26:46 -0500

    Started positioning items

--- src/components/FacesPage/FacesPage.js
+++ src/components/FacesPage/FacesPage.js
@@ -3,6 +3,25 @@ import "./FacesPage.css";
 
 const FacesPage = () => (
   <main id="faces">
+    <h1 className="faces-title">Meet the team</h1>
+
+    <section className="team">
+      <div className="team-card">
+        <img className="team-img" src="" alt=""/>
+        <h3 className="team-name">John</h3>
+        <p className="team-desc"></p>
+      </div>
+      <div className="team-card">
+        <img className="team-img" src="" alt=""/>
+        <h3 className="team-name">John</h3>
+        <p className="team-desc"></p>
+      </div>
+      <div className="team-card">
+        <img className="team-img" src="" alt=""/>
+        <h3 className="team-name">John</h3>
+        <p className="team-desc"></p>
+      </div>
+    </section>
   </main>
 );
 
